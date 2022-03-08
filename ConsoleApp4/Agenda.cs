227fe288f22cf7eb466721dee5ce52d7d66458b6
laurammorais <laulaumorais@gmail.com>
2022-03-08 13:37:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/Agenda.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp4
+{
+    public class Agenda
+    {
+        public Agenda(string nome, string telefone, int idade, string sexo)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Idade = idade;
+            Sexo = sexo;
+        }
+
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public int Idade { get; set; }
+        public string Sexo { get; set; }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda[] contatos = new Agenda[5];
+            int opcao = 0, indice = 0;
+
+            while (opcao != 4)
+            {
+                Console.WriteLine("---- Menu ----");
+                Console.WriteLine("1 - Inserir Contato");
+                Console.WriteLine("2 - Imprimir os Contatos");
+                Console.WriteLine("3 - Buscar Contato");
+                Console.WriteLine("4 - Sair");
+                opcao = int.Parse(Console.ReadLine());
+
+                if (opcao == 1)
+                {
+                    if (indice < 5)
+                    {
+                        Console.Write("nome: ");
+                        string nome = Console.ReadLine();
+                        Console.Write("telefone: ");
+                        string telefone = Console.ReadLine();
+                        Console.Write("idade: ");
+                        int idade = int.Parse(Console.ReadLine());
+                        Console.Write("sexo: ");
+                        string sexo = Console.ReadLine();
+
+                        Agenda contato = new Agenda(nome, telefone, idade, sexo);
+                        contatos[indice] = contato;
+                        indice++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Agenda cheia!");
+                    }
+                }
+                else if (opcao == 2)
+                {
+                    for (int x = 0; x < indice; x++)
+                    {
+                        Console.WriteLine($"\nnome: {contatos[x].Nome}\ntelefone: {contatos[x].Telefone}\nidade: {contatos[x].Idade}\nsexo: {contatos[x].Sexo}\n");
+                    }
+                }
+                else if (opcao == 3)
+                {
+                    Console.Write("Buscar nome: ");
+                    string nome = Console.ReadLine();
+
+                    for (int x = 0; x < indice; x++)
+                    {
+                        if (contatos[x].Nome == nome)
+                        {
+                            Console.WriteLine($"nome: {contatos[x].Nome}\ntelefone: {contatos[x].Telefone}\nidade: {contatos[x].Idade}\nsexo: {contatos[x].Sexo}\n");
+                        }
+                    }
+                }
+                else if (opcao == 4)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Opção não encontrada!");
+                }
+            }
+        }
+    }
+}
